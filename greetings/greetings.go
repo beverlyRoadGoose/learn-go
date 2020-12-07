e5c61d8050b4c5168001ccc16d1273a2e16fb683
Oluwatobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2020-12-07 22:21:20 +0100

    Use random response messages

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,10 @@
 package greetings
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person.
@@ -18,10 +20,32 @@ func Hello(name string) (string, error) {
 	 * you might have written this as:
 	 *
 	 * var message string
-	 * message = fmt.Sprintf("Hi, %v. Welcome!", name)
+	 * message = fmt.Sprintf(randomFormat(), name)
 	 */
 
-	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	// Create a message using a random format.
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// init sets initial values for variables used in the function.
+// Go executes init functions automatically at program startup, after global variables have been initialized.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+// lowercased functions are accessible only to code in its their package (in other words, they are not exported).
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
