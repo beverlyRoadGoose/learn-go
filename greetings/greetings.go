c8b47b265a9b00ddbcc4bba000b888a124837fe6
Oluwatobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2020-12-07 21:04:49 +0100

    Apply error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	/*
 	 * In Go, the := operator is a shortcut for declaring and initializing a variable in one line (Go uses the value on
 	 * the right to determine the variable's type). Taking the long way, you might have written this as:
@@ -14,5 +22,5 @@ func Hello(name string) string {
 
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"heytobi.dev/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Golang")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Golang")
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
