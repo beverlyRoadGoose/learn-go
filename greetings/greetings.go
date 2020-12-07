a46fc2b3d91cea5585d38e1beb9b607876f78183
Oluwatobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2020-12-07 21:32:42 +0100

    Code formatting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -13,8 +13,9 @@ func Hello(name string) (string, error) {
 	}
 
 	/*
-	 * In Go, the := operator is a shortcut for declaring and initializing a variable in one line (Go uses the value on
-	 * the right to determine the variable's type). Taking the long way, you might have written this as:
+	 * In Go, the := operator is a shortcut for declaring and initializing a variable in one line
+	 * (Go uses the value on the right to determine the variable's type). Taking the long way,
+	 * you might have written this as:
 	 *
 	 * var message string
 	 * message = fmt.Sprintf("Hi, %v. Welcome!", name)
